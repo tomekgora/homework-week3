4fd292cb0183f92ee575ac78b75a0a99cef15347
tomekgora <tomekgora@gmail.com>
2019-11-23 11:36:38 +0100

    added buttons to Quote.js, inline with author

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import "./Quote.css"
 
 export default class Quote extends Component {
+    state = {
+        liked: null
+    }
 
     render () {
         return (
           <div className="quote">
             <p>{this.props.text}</p>
-            <p>By: {this.props.author}</p>
+            <p className="author-and-buttons">By: {this.props.author}</p>
+            <button className="author-and-buttons">:)</button>
+            <button className="author-and-buttons">:(</button>
               
           </div>
         )
