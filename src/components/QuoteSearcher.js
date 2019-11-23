cd5963eea8171d0778988f9273108936e94b1846
tomekgora <tomekgora@gmail.com>
2019-11-23 15:07:16 +0100

    wrote countLikesDislikes fn, likedness and key are passed to Quote

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -14,15 +14,15 @@ export default class QuoteSearcher extends Component {
         // insert conditional statement for fetching false/true
         .then(incomingData => incomingData.json())
         .then(incomingData => {
-            console.log("why is incomingData undefined here?", incomingData)
+            console.log("why does incomingData come up undefined here?", incomingData)
             const processedData = incomingData
                 .results
                     .map(quote => {
                         return {...quote, likedStatus: null};
                     })
             return processedData
-        });
-        
+        })
+
         .then(incomingData => {
             console.log("is my likedStatus added?", incomingData)
             this.updateQuotes(incomingData)
@@ -31,12 +31,30 @@ export default class QuoteSearcher extends Component {
 
         .catch(console.error)
     }
-
+    // _proto_ of fetched data still comes up in the state.quotes, should it?
     updateQuotes(incomingData) {
         this.setState({
             quotes: incomingData
         })
     }
+
+    countLikeDislike () {
+        this.state.quotes.likedStatus.reduce((acc,likedOrDisliked) => {
+            if(likedOrDisliked === true) {
+                acc.likes += 1
+            } else if (likedOrDisliked === false) {
+                acc.dislikes += 1
+            } else {
+                // do nothing?
+            }
+            return acc
+        },{
+            likes: 0,
+            dislikes: 0
+        }); // end of reduce 
+    }
+
+
 
     render () {
         return (
@@ -46,7 +64,8 @@ export default class QuoteSearcher extends Component {
             ? "Loading..."
             :   <ul>
                     {this.state.quotes.map(quote =>
-                    <Quote 
+                    <Quote
+                    likedness={quote.likedStatus}
                     text={quote.quoteText}
                     author={quote.quoteAuthor}
                     key={quote._id}/>)}
